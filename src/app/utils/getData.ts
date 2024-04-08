45ca890143ff12336e647032925826e2cecd3737
Teemu Koivumaa <teemuk15@gmail.com>
2024-04-08 19:49:14 +0300

    feat: add toggle for automatic generation & append generation to real data

--- src/app/utils/getData.ts
+++ src/app/utils/getData.ts
@@ -22,25 +22,48 @@ export function setTableColumn(column: string, state: boolean) {
   localStorage.setItem("table-settings", JSON.stringify(newTableSettings));
 }
 
-export async function fetchHistory() {
+export async function fetchHistory(firstHistory: string) {
+  console.debug(firstHistory);
   const financials: [Financial] = getData();
 
-  const today = new Date();
-  const fiveDaysAgo = new Date(today);
-  fiveDaysAgo.setDate(fiveDaysAgo.getDate() - 5);
-  const formattedDate = `${fiveDaysAgo.getFullYear()}-${(
-    fiveDaysAgo.getMonth() + 1
+  let inputDate = null;
+  let formattedInput = null;
+  if (firstHistory) {
+    const [day, month, year] = firstHistory.split(".").map(Number);
+    // Create a new date object from the parsed input date
+    inputDate = new Date(year, month - 1, day);
+    formattedInput = `${inputDate.getFullYear()}-${(inputDate.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${inputDate.getDate().toString().padStart(2, "0")}`;
+  } else {
+    inputDate = new Date();
+  }
+
+  // Create a new date object 30 days ago from the input date
+  const thirtyDaysAgo: Date = new Date(inputDate);
+  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+  const formattedDate = `${thirtyDaysAgo.getFullYear()}-${(
+    thirtyDaysAgo.getMonth() + 1
   )
     .toString()
-    .padStart(2, "0")}-${fiveDaysAgo.getDate().toString().padStart(2, "0")}`;
+    .padStart(2, "0")}-${thirtyDaysAgo.getDate().toString().padStart(2, "0")}`;
+
+  console.log(formattedDate);
+  console.log(formattedInput);
 
   let newHistory: { value: number; date: any }[] = [];
 
   for (const financial of financials) {
     const ticker = financial.ticker;
-    const response = await fetch(`/api/history/${ticker}/${formattedDate}`, {
-      next: { revalidate: 3600 },
-    });
+    const response = await fetch(
+      formattedInput
+        ? `/api/history/${ticker}/${formattedDate}/${formattedInput}`
+        : `/api/history/${ticker}/${formattedDate}`,
+      {
+        next: { revalidate: 3600 },
+      }
+    );
     const financialHistories = await response.json();
 
     financialHistories.map((financialHistory: { close: any; date: any }) => {
@@ -51,6 +74,8 @@ export async function fetchHistory() {
       newHistory.push({ value: price, date: date });
     });
   }
+
+  console.debug(newHistory);
 
   return newHistory;
 }
